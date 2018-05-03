6d273ebcc264f9e4519300499014dcd622312820
natoszme <natoszmedra@gmail.com>
2018-05-03 17:15:54 -0300

    Se agrega lo BASICO para manejar la fecha de JSON. No puedo seguir mucho porque los tests siguen rompiendo por lo del hashmap (ver mail a Juan)

--- src/main/java/json/JSONParser.java
+++ src/main/java/json/JSONParser.java
@@ -8,6 +8,9 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 public class JSONParser<Entidad> {	
 	 
@@ -15,11 +18,14 @@ public class JSONParser<Entidad> {
 	public List<Entidad> importar(String rutaArchivo) {	
 		
 		File archivoJson = new File(rutaArchivo);
-		ObjectMapper mapper = new ObjectMapper();
+		ObjectMapper mapper = new ObjectMapper()
+					.registerModule(new ParameterNamesModule())
+					.registerModule(new Jdk8Module())
+					.registerModule(new JavaTimeModule());
 		List<Entidad> importados = new ArrayList<>();
 		
 		try {
-			importados = mapper.readValue(archivoJson, new TypeReference<List<Entidad>>(){});
+			importados = mapper.readValue(archivoJson, new TypeReference<ArrayList<Entidad>>(){});
 		} catch (JsonParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
